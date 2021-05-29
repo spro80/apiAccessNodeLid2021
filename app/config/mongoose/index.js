c3832745aae4859524e212f763b2d2c387bace37
David D <dsdiazs@falabella.cl>
2021-05-29 04:23:37 -0400

    add variables of connection for mongo from config by environment

--- app/config/mongoose/index.js
+++ app/config/mongoose/index.js
@@ -1,9 +1,14 @@
 'use strict';
+
+const config = require('../../config/index');
 const mongoose = require('mongoose');
+
+//const mongoConnection = `mongodb://localhost:27017/admin`;
+const mongoConnection = `mongodb://${config.mongoDB.ip}:${config.mongoDB.port}/${config.mongoDB.db}`;  
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/admin', {useMongoClient: true} )
+mongoose.connect(mongoConnection, {useMongoClient: true} )
   .then(() => {
     console.log("Conection to Mongo DB is succesfuly");
   })
