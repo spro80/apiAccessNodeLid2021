b5af6daf1d534a310db34928247fb86b1bfcac95
David D <dsdiazs@falabella.cl>
2021-05-29 03:33:09 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+// ddos //denial of service
+const config = require('./app/config/index');
+
+const RouteV1 = require(`./app/routes/${config.envApp}/v1/route`); // eslint-disable-line
+
+const app = express();
+
+app.set('trust proxy', true);
+
+//const ddos = new Ddos({ burst: 10, limit: 15 }, true);
+//app.use(ddos.express);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.send('Hello Docker\n');
+});
+
+// health check MS
+app.get('/api/products/health', (req, res) => {
+  res.send(`${config.msConfig.name} up and running`);
+});
+
+app.use('/api/products',
+  RouteV1);
+
+
+app.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).send({ code: 'error', message: 'internal error not handled' });
+});
+
+module.exports = app;
